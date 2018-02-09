c11f0aa9d3a326122b13149a2c5b0956d1061714
R. Oliveira Narasaki <r.oliveira.narasaki@avanade.com>
2018-02-09 18:50:53 -0200

    create action | redux flow

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import {Route, IndexRoute} from 'react-router';
 import App from "./components/App";
 import HomePage from "./components/home/HomePage";
 import  AboutPage from "./components/about/AboutPage";
-import coursePage from './components/course/coursePage';
+import coursePage from './components/courses/coursePage';
 
 export default (
     <Route path="/" component={App} >
         <IndexRoute component={HomePage} />
-        <Route path="course" component={coursePage} />
+        <Route path="courses" component={coursePage} />
         <Route path="about" component={AboutPage} />
     </Route>
 );
