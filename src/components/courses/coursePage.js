352af19707e9e9561b28812849a3040d1f8317be
R. Oliveira Narasaki <r.oliveira.narasaki@avanade.com>
2018-02-10 13:33:38 -0200

    create Manage Course Page

--- src/components/courses/coursePage.js
+++ src/components/courses/coursePage.js
@@ -2,27 +2,12 @@ import React, {PropTypes, Component} from 'react';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseAction from '../../actions/courseAction';
+import CourseList from './CourseList';
 
 class CoursesPage extends Component{
 
     constructor(props, context){
         super(props, context);
-        this.state = {
-            course : {title : ""}
-        };
-
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    onTitleChange(event){
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course : course});
-    }
-
-    onClickSave(event){
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index){
@@ -30,19 +15,11 @@ class CoursesPage extends Component{
     }
 
     render(){
+        const {courses} = this.props
         return(
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h2>Add Course </h2>
-                <input 
-                 type="text"
-                 onChange={this.onTitleChange}
-                 value={this.state.course.title}/>
-                <input 
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave} />
+                <CourseList courses={courses} />
             </div>
 
         );
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from "./components/App";
 import HomePage from "./components/home/HomePage";
 import  AboutPage from "./components/about/AboutPage";
 import coursePage from './components/courses/coursePage';
+import ManageCoursePage from './components/courses/ManageCoursePage';
 
 export default (
     <Route path="/" component={App} >
         <IndexRoute component={HomePage} />
         <Route path="courses" component={coursePage} />
+        <Route path="course" component={ManageCoursePage} />
+        <Route path="course/:id" component={ManageCoursePage} />
         <Route path="about" component={AboutPage} />
     </Route>
 );
