cd195ebee5fec9ceb17d72f07da51e182e38e242
R. Oliveira Narasaki <r.oliveira.narasaki@avanade.com>
2018-02-10 13:32:41 -0200

    mockApi

--- src/actions/CourseAction.js
+++ src/actions/CourseAction.js
@@ -1,4 +1,21 @@
 import * as types from './actionsTypes' ;
+import courseApi from '../api/mockCourseApi';
+
+
+export function loadCoursesSuccess(course){
+    return { type: types.LOAD_COURSES_SUCCESS , course};
+}
+
+export function loadCourses(){
+    return function(dispatch){
+        return courseApi.getAllCourses().then(courses =>{
+            dispatch(loadCoursesSuccess(courses));
+        }).catch(error => {
+            throw(error);
+        });
+    }
+}
+
 export function createCourse(course){
     return { type: types.CREATE_COURSE , course};
 }
