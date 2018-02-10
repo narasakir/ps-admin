d9ecfa99165cd12f1c9557b13b14ed3df31bfb08
R. Oliveira Narasaki <r.oliveira.narasaki@avanade.com>
2018-02-10 13:32:15 -0200

    mock api

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
 import * as types from '../actions/actionsTypes' ;
 export default function courseReducer(state = [], action){
     switch(action.type){
+        case types.LOAD_COURSES_SUCCESS:
+            return action.course;
         case types.CREATE_COURSE : 
             return [...state,
                 Object.assign({}, action.course)
