bd40a5865ffb1b6ea4f8cc5aa7e71fcb79c4f64e
R. Oliveira Narasaki <r.oliveira.narasaki@avanade.com>
2018-02-12 13:15:34 -0200

    tests on reducers | components | stores

---
+++ src/selectors/selector.js
@@ -0,0 +1,9 @@
+
+export function authorsFormattedForDropdown(authors) {
+    return authors.map(author => {
+      return {
+        value: author.id,
+        text: author.firstName + ' ' + author.lastName
+      };
+    });
+  }
